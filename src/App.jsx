7362f24bad1a0d62e1b8ade34243d7dd13cadff5
ayuko399 <tok392870@gmail.com>
2025-02-17 14:00:54 +0000

    delete lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
+
 
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app">
-        <h1>NC News</h1>
-        <Routes>
-          <Route path="/" element={<h2>Homepage will go here</h2>} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+
   );
 }
 
